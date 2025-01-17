f237a0a391d11713764834cb02c554d6e285e9cc
Toby Wise <tobyw31@gmail.com>
2025-01-17 16:10:10 +0000

    Test for softmax with inverse temperature

--- tests/test_decision_rules.py
+++ tests/test_decision_rules.py
@@ -3,7 +3,7 @@ import jax.numpy as jnp
 import jax
 from typing import List
 import sys
-from behavioural_modelling.decision_rules import softmax, softmax_subtract_max
+from behavioural_modelling.decision_rules import softmax, softmax_subtract_max, softmax_inverse_temperature
 
 
 def test_softmax():
@@ -80,3 +80,29 @@ def test_softmax_subtract_max_extreme_values():
 
     # Check that there are no NaNs or Infs
     assert jnp.all(jnp.isfinite(softmax_probs))
+
+def test_softmax_inverse_temperature():
+    # Generate a range of values from -1 to 1 for a single trial
+    test_values = jnp.linspace(-1, 1, 100)[:, None]
+
+    # Add 1-value as a second option
+    test_values = jnp.hstack((test_values, (2 - (1 + test_values)) - 1))
+
+    # Define inverse temperatures
+    inverse_temperatures = [0.25, 0.5, 1, 2, 5, 10]
+
+    # Calculate softmax probabilities for each inverse temperature
+    softmax_probs = jnp.array(
+        [softmax_inverse_temperature(test_values, inverse_temperature=t) for t in inverse_temperatures]
+    )
+
+    # Check that probabilities sum to 1
+    assert jnp.allclose(softmax_probs.sum(axis=2), 1)
+
+    # Check that probabilities are monotonically increasing with value
+    assert jnp.all(jnp.diff(softmax_probs[..., 0], axis=-1) >= 0)
+
+    # Check that probability curves get steeper with increasing inverse temperature
+    # (opposite of regular softmax temperature)
+    assert jnp.all(jnp.diff(jnp.diff(softmax_probs[:, :, 0], axis=-1).sum(axis=-1)) > 0)
+
