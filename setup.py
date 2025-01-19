29c56c13aba29ca33ed5f2c403cb382e2112466d
Toby Wise <tobyw31@gmail.com>
2025-01-19 11:42:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='behavioural_modelling',
+    name="behavioural_modelling",
+    version="0.1.0",
+    description="A package for behavioural modelling",
     packages=find_packages(),
-    version='0.1.0',
-    description='A package for behavioural modelling',
-    requires=['jax']
+    install_requires=[
+        "jax",
+        "jaxlib", 
+        "numpy",
+    ],
+    tests_require=[
+        "pytest",
+    ],
+    extras_require={
+        "test": ["pytest"],
+    }
 )
